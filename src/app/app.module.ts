8a6e9971c53225fb3b99587e786624d7a698bae3
Julio Cesar <jucema89@gmail.com>
2019-03-15 16:37:56 -0500

    Subida de Archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
+
+// LLAMA LA RUTA PARA EL ROUTEO
+import { RouterModule, Routes } from '@angular/router';
+// TERMINA EL LLAMADO
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeBannerComponent } from './componentes/home-banner/home-banner.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+
+// PROCESO DE CONSTANTES PARA EL ROUTEO//
+const routes: Routes = [
+  { path: '', component: HomeBannerComponent },
+  { path: 'sobremi', component: SobremiComponent },
+  { path: 'portafolio', component: PortafolioComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: ''},
+
+
+];
+
+// FIN PROCESO DE CONSTANTES PARA EL ROUTEO//
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeBannerComponent,
+    ContactoComponent,
+    SobremiComponent,
+    PortafolioComponent,
+    ServiciosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Importa el Modulo de Ruteo
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled'})
+    // Fin de Importe de Modulo
   ],
   providers: [],
   bootstrap: [AppComponent]
